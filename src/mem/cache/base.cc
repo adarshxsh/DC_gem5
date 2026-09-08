255ea8b3cf7a100461934dfcf1c36675424ada24
Adarsh <udhabhaw@gmail.com>
2026-09-08 23:38:36 +0000

    mem-cache: Implement non-downgrading prefetch co-allocation guard

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -1696,7 +1696,8 @@ BaseCache::allocateBlock(const PacketPtr pkt, PacketList &writebacks)
     // Find replacement victim
     std::vector<CacheBlk*> evict_blks;
     CacheBlk *victim = tags->findVictim({addr, is_secure}, blk_size_bits,
-                                        evict_blks, partition_id);
+                                        evict_blks, partition_id,
+                                        pkt->isPrefetch());
 
     // It is valid to return nullptr if there is no victim
     if (!victim)
