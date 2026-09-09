b431fe49a834f01e887b265a4ccaad2d0f64ce85
Adarsh <udhabhaw@gmail.com>
2026-09-09 22:02:57 +0000

    mem: Implement variable-length DRAM burst scheduling in MemCtrl and DRAMInterface

--- src/mem/packet.hh
+++ src/mem/packet.hh
@@ -396,6 +396,9 @@ class Packet : public Printable, public Extensible<Packet>
     /// The size of the request or transfer.
     unsigned size;
 
+    /// The compressed size of the request payload in bytes (0 if uncompressed).
+    unsigned _compressedSize;
+
     /**
      * Track the bytes found that satisfy a functional read.
      */
@@ -817,6 +820,28 @@ class Packet : public Printable, public Extensible<Packet>
     unsigned getSize() const  { assert(flags.isSet(VALID_SIZE)); return size; }
 
     /**
+     * Get the compressed size of the request payload in bytes.
+     * If no compressed size is set, defaults to packet size.
+     */
+    unsigned getCompressedSize() const
+    {
+        return std::min(getSize(), _compressedSize ? _compressedSize : getSize());
+    }
+
+    /**
+     * Set the compressed size of the request payload in bytes.
+     */
+    void setCompressedSize(unsigned csize) { _compressedSize = csize; }
+
+    /**
+     * Return true if the packet payload is compressed.
+     */
+    bool isCompressed() const
+    {
+        return _compressedSize > 0 && _compressedSize < getSize();
+    }
+
+    /**
      * Get address range to which this packet belongs.
      *
      * @return Address range of this packet.
@@ -877,6 +902,7 @@ class Packet : public Printable, public Extensible<Packet>
     Packet(const RequestPtr &_req, MemCmd _cmd)
         :  cmd(_cmd), id((PacketId)_req.get()), req(_req),
            data(nullptr), addr(0), _isSecure(false), size(0),
+           _compressedSize(0),
            _qosValue(0),
            htmReturnReason(HtmCacheFailure::NO_FAIL),
            htmTransactionUid(0),
@@ -918,6 +944,7 @@ class Packet : public Printable, public Extensible<Packet>
     Packet(const RequestPtr &_req, MemCmd _cmd, int _blkSize, PacketId _id = 0)
         :  cmd(_cmd), id(_id ? _id : (PacketId)_req.get()), req(_req),
            data(nullptr), addr(0), _isSecure(false),
+           _compressedSize(0),
            _qosValue(0),
            htmReturnReason(HtmCacheFailure::NO_FAIL),
            htmTransactionUid(0),
@@ -946,6 +973,7 @@ class Packet : public Printable, public Extensible<Packet>
            cmd(pkt->cmd), id(pkt->id), req(pkt->req),
            data(nullptr),
            addr(pkt->addr), _isSecure(pkt->_isSecure), size(pkt->size),
+           _compressedSize(pkt->_compressedSize),
            bytesValid(pkt->bytesValid),
            _qosValue(pkt->qosValue()),
            htmReturnReason(HtmCacheFailure::NO_FAIL),
