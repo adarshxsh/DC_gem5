60a7b3e8bb49d6b0255c2eca54f7a3e75daaef13
Adarsh <udhabhaw@gmail.com>
2026-09-09 22:22:41 +0000

    style: apply clang-format to packet payload size changes

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -1798,7 +1798,8 @@ BaseCache::writebackBlk(CacheBlk *blk)
     // sent for writeback.
     if (compressor) {
         pkt->headerDelay += compressor->getDecompressionLatency(blk);
-        const CompressionBlk* comp_blk = static_cast<const CompressionBlk*>(blk);
+        const CompressionBlk *comp_blk =
+            static_cast<const CompressionBlk *>(blk);
         if (comp_blk && comp_blk->isCompressed()) {
             pkt->setPayloadSize(divCeil(comp_blk->getSizeBits(), 8));
         }
@@ -1847,7 +1848,8 @@ BaseCache::writecleanBlk(CacheBlk *blk, Request::Flags dest, PacketId id)
     // sent for writeback.
     if (compressor) {
         pkt->headerDelay += compressor->getDecompressionLatency(blk);
-        const CompressionBlk* comp_blk = static_cast<const CompressionBlk*>(blk);
+        const CompressionBlk *comp_blk =
+            static_cast<const CompressionBlk *>(blk);
         if (comp_blk && comp_blk->isCompressed()) {
             pkt->setPayloadSize(divCeil(comp_blk->getSizeBits(), 8));
         }
