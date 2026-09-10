c7ba770492b8e737435d4c80780c6506f4d61d5f
Adarsh <udhabhaw@gmail.com>
2026-09-10 23:27:36 +0000

    mem: migrate writeback decompression latency and use compressed payload timing

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -1796,7 +1796,11 @@ BaseCache::writebackBlk(CacheBlk *blk)
     // When a block is compressed, it must first be decompressed before being
     // sent for writeback.
     if (compressor) {
-        pkt->payloadDelay = compressor->getDecompressionLatency(blk);
+        pkt->headerDelay += cyclesToTicks(compressor->getDecompressionLatency(blk));
+        const CompressionBlk* comp_blk = static_cast<const CompressionBlk*>(blk);
+        if (comp_blk && comp_blk->isCompressed()) {
+            pkt->setCompressedSize(divCeil(comp_blk->getSizeBits(), CHAR_BIT));
+        }
     }
 
     return pkt;
@@ -1841,7 +1845,11 @@ BaseCache::writecleanBlk(CacheBlk *blk, Request::Flags dest, PacketId id)
     // When a block is compressed, it must first be decompressed before being
     // sent for writeback.
     if (compressor) {
-        pkt->payloadDelay = compressor->getDecompressionLatency(blk);
+        pkt->headerDelay += cyclesToTicks(compressor->getDecompressionLatency(blk));
+        const CompressionBlk* comp_blk = static_cast<const CompressionBlk*>(blk);
+        if (comp_blk && comp_blk->isCompressed()) {
+            pkt->setCompressedSize(divCeil(comp_blk->getSizeBits(), CHAR_BIT));
+        }
     }
 
     return pkt;
