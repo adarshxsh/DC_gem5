c2930c879ec0e38ead2807a45543e7e739df7e20
Adarsh <udhabhaw@gmail.com>
2026-09-10 23:01:54 +0000

    mem: Propagate compressed payload size to MemCtrl and DRAMInterface for dynamic tBURST scaling

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -332,6 +332,11 @@ BaseCache::handleTimingReqHit(PacketPtr pkt, CacheBlk *blk, Tick request_time)
         assert(pkt->headerDelay == 0);
         assert(pkt->payloadDelay == 0);
 
+        CompressionBlk *cblk = dynamic_cast<CompressionBlk *>(blk);
+        if (cblk && cblk->isCompressed()) {
+            pkt->setCompressedSizeBits(cblk->getSizeBits());
+        }
+
         pkt->makeTimingResponse();
 
         // In this case we are considering request_time that takes
@@ -1799,6 +1804,11 @@ BaseCache::writebackBlk(CacheBlk *blk)
         pkt->payloadDelay = compressor->getDecompressionLatency(blk);
     }
 
+    CompressionBlk *cblk = dynamic_cast<CompressionBlk *>(blk);
+    if (cblk && cblk->isCompressed()) {
+        pkt->setCompressedSizeBits(cblk->getSizeBits());
+    }
+
     return pkt;
 }
 
@@ -1842,6 +1852,10 @@ BaseCache::writecleanBlk(CacheBlk *blk, Request::Flags dest, PacketId id)
     // sent for writeback.
     if (compressor) {
         pkt->payloadDelay = compressor->getDecompressionLatency(blk);
+    }
+
+    if (cblk && cblk->isCompressed()) {
+        pkt->setCompressedSizeBits(cblk->getSizeBits());
     }
 
     return pkt;
