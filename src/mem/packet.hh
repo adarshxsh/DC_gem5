e85649563088ce9765daf8e9751f09d8913dbd2c
Adarsh <udhabhaw@gmail.com>
2026-09-12 02:34:18 +0000

    mem: Decouple endpoint decompression latency from crossbar payload delay
    - Add compressed payload size tracking and decompression delay accessors to Packet.
    - Compute crossbar payload transfer duration in BaseXBar::calcPacketTiming using Packet::getPayloadSize().
    - Remove decompression latency assignment to pkt->payloadDelay in BaseCache::writebackBlk and BaseCache::writecleanBlk.
    - Absorb endpoint decompression delay locally in BaseCache::allocateWriteBuffer event scheduling prior to packet release.

--- src/mem/packet.hh
+++ src/mem/packet.hh
@@ -396,6 +396,12 @@ class Packet : public Printable, public Extensible<Packet>
     /// The size of the request or transfer.
     unsigned size;
 
+    /// The size of the compressed payload (in bytes).
+    unsigned _payloadSize;
+
+    /// Endpoint decompression delay (in ticks).
+    uint32_t _decompressionDelay;
+
     /**
      * Track the bytes found that satisfy a functional read.
      */
@@ -816,6 +822,12 @@ class Packet : public Printable, public Extensible<Packet>
 
     unsigned getSize() const  { assert(flags.isSet(VALID_SIZE)); return size; }
 
+    void setPayloadSize(unsigned size) { _payloadSize = size; }
+    unsigned getPayloadSize() const { return _payloadSize ? _payloadSize : getSize(); }
+
+    void setDecompressionDelay(uint32_t delay) { _decompressionDelay = delay; }
+    uint32_t getDecompressionDelay() const { return _decompressionDelay; }
+
     /**
      * Get address range to which this packet belongs.
      *
@@ -877,6 +889,7 @@ class Packet : public Printable, public Extensible<Packet>
     Packet(const RequestPtr &_req, MemCmd _cmd)
         :  cmd(_cmd), id((PacketId)_req.get()), req(_req),
            data(nullptr), addr(0), _isSecure(false), size(0),
+           _payloadSize(0), _decompressionDelay(0),
            _qosValue(0),
            htmReturnReason(HtmCacheFailure::NO_FAIL),
            htmTransactionUid(0),
@@ -918,6 +931,7 @@ class Packet : public Printable, public Extensible<Packet>
     Packet(const RequestPtr &_req, MemCmd _cmd, int _blkSize, PacketId _id = 0)
         :  cmd(_cmd), id(_id ? _id : (PacketId)_req.get()), req(_req),
            data(nullptr), addr(0), _isSecure(false),
+           _payloadSize(0), _decompressionDelay(0),
            _qosValue(0),
            htmReturnReason(HtmCacheFailure::NO_FAIL),
            htmTransactionUid(0),
@@ -946,6 +960,8 @@ class Packet : public Printable, public Extensible<Packet>
            cmd(pkt->cmd), id(pkt->id), req(pkt->req),
            data(nullptr),
            addr(pkt->addr), _isSecure(pkt->_isSecure), size(pkt->size),
+           _payloadSize(pkt->_payloadSize),
+           _decompressionDelay(pkt->_decompressionDelay),
            bytesValid(pkt->bytesValid),
            _qosValue(pkt->qosValue()),
            htmReturnReason(HtmCacheFailure::NO_FAIL),
