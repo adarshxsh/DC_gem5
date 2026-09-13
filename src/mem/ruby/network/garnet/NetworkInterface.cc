cea3ded352f812d3c7a838ed6f7178f457511b17
Adarsh <udhabhaw@gmail.com>
2026-09-13 22:22:43 +0000

    mem, ruby: Decouple endpoint decompression latency and support compressed payload size

--- src/mem/ruby/network/garnet/NetworkInterface.cc
+++ src/mem/ruby/network/garnet/NetworkInterface.cc
@@ -384,10 +384,10 @@ NetworkInterface::flitisizeMessage(MsgPtr msg_ptr, int vnet)
     OutputPort *oPort = getOutportForVnet(vnet);
     assert(oPort);
     int num_flits = (int)divCeil((float) m_net_ptr->MessageSizeType_to_int(
-        net_msg_ptr->getMessageSize()), (float)oPort->bitWidth());
+        net_msg_ptr->getMessageSize(), net_msg_ptr), (float)oPort->bitWidth());
 
     DPRINTF(RubyNetwork, "Message Size:%d vnet:%d bitWidth:%d\n",
-        m_net_ptr->MessageSizeType_to_int(net_msg_ptr->getMessageSize()),
+        m_net_ptr->MessageSizeType_to_int(net_msg_ptr->getMessageSize(), net_msg_ptr),
         vnet, oPort->bitWidth());
 
     // loop to convert all multicast messages into unicast messages
@@ -451,7 +451,7 @@ NetworkInterface::flitisizeMessage(MsgPtr msg_ptr, int vnet)
             flit *fl = new flit(packet_id,
                 i, vc, vnet, route, num_flits, new_msg_ptr,
                 m_net_ptr->MessageSizeType_to_int(
-                net_msg_ptr->getMessageSize()),
+                net_msg_ptr->getMessageSize(), net_msg_ptr),
                 oPort->bitWidth(), curTick());
 
             fl->set_src_delay(curTick() - msg_ptr->getTime());
