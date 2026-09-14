ac19b2b1e8fe145c35b1115bbddf4c0c196f32ea
Adarsh <udhabhaw@gmail.com>
2026-09-14 21:37:02 +0000

    mem: enhance crossbar layer and Garnet VC arbitration with packet priority and decompression penalty weighting

--- src/mem/hmc_controller.cc
+++ src/mem/hmc_controller.cc
@@ -51,7 +51,7 @@ bool HMCController::recvTimingReq(PacketPtr pkt, PortID cpu_side_port_id)
 
     // test if the layer should be considered occupied for the current
     // port
-    if (!reqLayers[mem_side_port_id]->tryTiming(src_port)) {
+    if (!reqLayers[mem_side_port_id]->tryTiming(src_port, pkt)) {
         DPRINTF(HMCController, "recvTimingReq: src %s %s 0x%x BUSY\n",
                 src_port->name(), pkt->cmdString(), pkt->getAddr());
         return false;
