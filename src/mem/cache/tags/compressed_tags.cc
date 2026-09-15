8527fe26fa506a12b26d0797cbc1f486e40556d6
Adarsh <udhabhaw@gmail.com>
2026-09-15 21:18:17 +0000

    mem-cache: SuperBlk accumulative bit co-allocation
    Replaces discrete compression factor count caps in SuperBlk::canCoAllocate with accumulative payload bit tracking against physical cache line capacity. Allows co-allocation of variable-ratio compressed sub-blocks whenever total payload bits and sub-block slots are available.

--- src/mem/cache/tags/compressed_tags.cc
+++ src/mem/cache/tags/compressed_tags.cc
@@ -45,6 +45,8 @@
 
 #include "mem/cache/tags/compressed_tags.hh"
 
+#include <climits>
+
 #include "base/trace.hh"
 #include "debug/CacheComp.hh"
 #include "mem/cache/replacement_policies/base.hh"
@@ -214,7 +216,7 @@ CompressedTags::checkInvariants() const
         if (super_blk.isValid()) {
             uint8_t num_valid = super_blk.getNumValid();
             uint8_t cf = super_blk.getCompressionFactor();
-            assert(num_valid <= cf);
+            std::size_t total_bits = 0;
             if (num_valid > 1) {
                 assert(super_blk.isCompressed());
             }
@@ -225,8 +227,10 @@ CompressedTags::checkInvariants() const
                     uint8_t blk_cf = super_blk.calculateCompressionFactor(
                         cblk->getSizeBits());
                     assert(blk_cf >= cf);
+                    total_bits += cblk->getSizeBits();
                 }
             }
+            assert(total_bits <= super_blk.getBlkSize() * CHAR_BIT);
         } else {
             assert(super_blk.getCompressionFactor() == 1);
         }
