ad18124e21d85137d38653e8d4a44460826bb5af
Adarsh <udhabhaw@gmail.com>
2026-09-17 00:27:37 +0000

    ruby, garnet: dynamically calculate flit count in NetworkInterface
    Modifies NetworkInterface::flitisizeMessage to dynamically calculate payload flit count based on actual compressed byte size reported by the message object instead of static message size types.

--- src/mem/ruby/network/garnet/NetworkInterface.cc
+++ src/mem/ruby/network/garnet/NetworkInterface.cc
@@ -383,11 +383,21 @@ NetworkInterface::flitisizeMessage(MsgPtr msg_ptr, int vnet)
     // This is expressed in terms of bytes/cycle or the flit size
     OutputPort *oPort = getOutportForVnet(vnet);
     assert(oPort);
-    int num_flits = (int)divCeil((float) m_net_ptr->MessageSizeType_to_int(
-        net_msg_ptr->getMessageSize()), (float)oPort->bitWidth());
+
+    int msg_size = net_msg_ptr->getPayloadSize();
+    if (msg_size >= 0) {
+        int header_overhead =
+            m_net_ptr->MessageSizeType_to_int(MessageSizeType_Control);
+        msg_size += header_overhead;
+    } else {
+        msg_size = m_net_ptr->MessageSizeType_to_int(
+            net_msg_ptr->getMessageSize());
+    }
+
+    int num_flits = (int)divCeil((float) msg_size, (float)oPort->bitWidth());
 
     DPRINTF(RubyNetwork, "Message Size:%d vnet:%d bitWidth:%d\n",
-        m_net_ptr->MessageSizeType_to_int(net_msg_ptr->getMessageSize()),
+        msg_size,
         vnet, oPort->bitWidth());
 
     // loop to convert all multicast messages into unicast messages
@@ -450,8 +460,7 @@ NetworkInterface::flitisizeMessage(MsgPtr msg_ptr, int vnet)
             m_net_ptr->increment_injected_flits(vnet);
             flit *fl = new flit(packet_id,
                 i, vc, vnet, route, num_flits, new_msg_ptr,
-                m_net_ptr->MessageSizeType_to_int(
-                net_msg_ptr->getMessageSize()),
+                msg_size,
                 oPort->bitWidth(), curTick());
 
             fl->set_src_delay(curTick() - msg_ptr->getTime());
