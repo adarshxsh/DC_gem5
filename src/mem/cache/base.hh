4f66d7829ef49bfca8957f6d4c5b6bb1af3c7583
Adarsh <udhabhaw@gmail.com>
2026-09-18 19:11:25 +0000

    style: apply clang-format to modified cache files

--- src/mem/cache/base.hh
+++ src/mem/cache/base.hh
@@ -1157,7 +1157,8 @@ class BaseCache : public ClockedObject
         /** Number of data expansions. */
         statistics::Scalar dataExpansions;
 
-        /** Number of partial sub-block evictions caused by block expansions. */
+        /** Number of partial sub-block evictions caused by block expansions.
+         */
         statistics::Scalar partialSubBlockEvictions;
 
         /**
