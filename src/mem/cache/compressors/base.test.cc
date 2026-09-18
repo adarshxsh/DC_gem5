fb403d4fabd160644d6684d01d3b9dbe5d367e66
Adarsh <udhabhaw@gmail.com>
2026-09-18 19:21:02 +0000

    [CI/CD Fix Attempt 1] style: apply clang-format to base.test.cc

--- src/mem/cache/compressors/base.test.cc
+++ src/mem/cache/compressors/base.test.cc
@@ -24,7 +24,8 @@
  * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */TEST_F(BaseCompressorTest, CumulativeStatsIndependence)
+ */
+TEST_F(BaseCompressorTest, CumulativeStatsIndependence)
 {
     auto compressor = createZeroCompressor(true, 1.5f, 1, 3);
 
