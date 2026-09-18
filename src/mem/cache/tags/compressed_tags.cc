54efaf7916e0f4fc7f4a740aa0446cd02b291115
Adarsh <udhabhaw@gmail.com>
2026-09-18 22:47:38 +0000

    fix: Mark assertion-only variables in CompressedTags::checkInvariants as [[maybe_unused]]

--- src/mem/cache/tags/compressed_tags.cc
+++ src/mem/cache/tags/compressed_tags.cc
@@ -214,9 +214,9 @@ CompressedTags::checkInvariants() const
     SectorTags::checkInvariants();
     for (const auto &super_blk : superBlks) {
         if (super_blk.isValid()) {
-            uint8_t num_valid = super_blk.getNumValid();
-            uint8_t cf = super_blk.getCompressionFactor();
-            std::size_t total_bits = 0;
+            [[maybe_unused]] uint8_t num_valid = super_blk.getNumValid();
+            [[maybe_unused]] uint8_t cf = super_blk.getCompressionFactor();
+            [[maybe_unused]] std::size_t total_bits = 0;
             if (num_valid > 1) {
                 assert(super_blk.isCompressed());
             }
@@ -225,8 +225,9 @@ CompressedTags::checkInvariants() const
                     const CompressionBlk *cblk =
                         static_cast<const CompressionBlk *>(blk);
                     total_bits += cblk->getSizeBits();
-                    uint8_t blk_cf = super_blk.calculateCompressionFactor(
-                        cblk->getSizeBits());
+                    [[maybe_unused]] uint8_t blk_cf =
+                        super_blk.calculateCompressionFactor(
+                            cblk->getSizeBits());
                     assert(blk_cf >= cf);
                 }
             }
