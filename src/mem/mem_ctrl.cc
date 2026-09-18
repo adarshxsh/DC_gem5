517af4af8d383e5ca45d110563a777015bd72511
Adarsh <udhabhaw@gmail.com>
2026-09-18 19:12:12 +0000

    style: format code with clang-format to fix CI clang-format-check

--- src/mem/mem_ctrl.cc
+++ src/mem/mem_ctrl.cc
@@ -57,30 +57,39 @@ namespace gem5
 namespace memory
 {
 
-MemCtrl::MemCtrl(const MemCtrlParams &p) :
-    qos::MemCtrl(p),
-    port(name() + ".port", *this), isTimingMode(false),
-    retryRdReq(false), retryWrReq(false),
-    nextReqEvent([this] {processNextReqEvent(dram, respQueue,
-                         respondEvent, nextReqEvent, retryWrReq);}, name()),
-    respondEvent([this] {processRespondEvent(dram, respQueue,
-                         respondEvent, retryRdReq); }, name()),
-    dram(p.dram),
-    readBufferSize(dram->readBufferSize),
-    writeBufferSize(dram->writeBufferSize),
-    writeHighThreshold(writeBufferSize * p.write_high_thresh_perc / 100.0),
-    writeLowThreshold(writeBufferSize * p.write_low_thresh_perc / 100.0),
-    minWritesPerSwitch(p.min_writes_per_switch),
-    minReadsPerSwitch(p.min_reads_per_switch),
-    memSchedPolicy(p.mem_sched_policy),
-    frontendLatency(p.static_frontend_latency),
-    backendLatency(p.static_backend_latency),
-    commandWindow(p.command_window),
-    prevArrival(0),
-    emaAlpha(p.ema_alpha),
-    smoothedRdQLen(0.0),
-    smoothedWrQLen(0.0),
-    stats(*this)
+MemCtrl::MemCtrl(const MemCtrlParams &p)
+    : qos::MemCtrl(p),
+      port(name() + ".port", *this),
+      isTimingMode(false),
+      retryRdReq(false),
+      retryWrReq(false),
+      nextReqEvent(
+          [this] {
+              processNextReqEvent(dram, respQueue, respondEvent, nextReqEvent,
+                                  retryWrReq);
+          },
+          name()),
+      respondEvent(
+          [this] {
+              processRespondEvent(dram, respQueue, respondEvent, retryRdReq);
+          },
+          name()),
+      dram(p.dram),
+      readBufferSize(dram->readBufferSize),
+      writeBufferSize(dram->writeBufferSize),
+      writeHighThreshold(writeBufferSize * p.write_high_thresh_perc / 100.0),
+      writeLowThreshold(writeBufferSize * p.write_low_thresh_perc / 100.0),
+      minWritesPerSwitch(p.min_writes_per_switch),
+      minReadsPerSwitch(p.min_reads_per_switch),
+      memSchedPolicy(p.mem_sched_policy),
+      frontendLatency(p.static_frontend_latency),
+      backendLatency(p.static_backend_latency),
+      commandWindow(p.command_window),
+      prevArrival(0),
+      emaAlpha(p.ema_alpha),
+      smoothedRdQLen(0.0),
+      smoothedWrQLen(0.0),
+      stats(*this)
 {
     DPRINTF(MemCtrl, "Setting up controller\n");
 
@@ -192,7 +201,8 @@ void
 MemCtrl::updateRdQueueEMA()
 {
     double current_len = totalReadQueueSize + respQueue.size();
-    smoothedRdQLen = emaAlpha * current_len + (1.0 - emaAlpha) * smoothedRdQLen;
+    smoothedRdQLen =
+        emaAlpha * current_len + (1.0 - emaAlpha) * smoothedRdQLen;
     stats.avgRdQLen = smoothedRdQLen;
 }
 
@@ -200,7 +210,8 @@ void
 MemCtrl::updateWrQueueEMA()
 {
     double current_len = totalWriteQueueSize;
-    smoothedWrQLen = emaAlpha * current_len + (1.0 - emaAlpha) * smoothedWrQLen;
+    smoothedWrQLen =
+        emaAlpha * current_len + (1.0 - emaAlpha) * smoothedWrQLen;
     stats.avgWrQLen = smoothedWrQLen;
 }
 
