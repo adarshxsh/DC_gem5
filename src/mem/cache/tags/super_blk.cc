0ba9eb6207c72e8da24c21d35f5ab0c000f20db6
Adarsh <udhabhaw@gmail.com>
2026-09-19 19:12:03 +0000

    style: format code with clang-format

--- src/mem/cache/tags/super_blk.cc
+++ src/mem/cache/tags/super_blk.cc
@@ -276,8 +276,7 @@ SuperBlk::canCoAllocate(const std::size_t compressed_size) const
 
     std::size_t bit_sum = 0;
     for (const auto &blk : blks) {
-        const CompressionBlk *cblk =
-            static_cast<const CompressionBlk *>(blk);
+        const CompressionBlk *cblk = static_cast<const CompressionBlk *>(blk);
         if (blk->isValid() || cblk->isReserved()) {
             bit_sum += cblk->getSizeBits();
         }
