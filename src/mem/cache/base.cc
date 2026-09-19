ac80c82a21e94e3bdb198aa8da9e449394f409a5
Adarsh <udhabhaw@gmail.com>
2026-09-19 19:20:15 +0000

    mem: migrate decompression latency to headerDelay
    Migrate decompression latency to headerDelay and use compressed transfer size.

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -47,6 +47,7 @@
 #include <algorithm>
 
 #include "base/compiler.hh"
+#include "base/intmath.hh"
 #include "base/logging.hh"
 #include "debug/Cache.hh"
 #include "debug/CacheComp.hh"
@@ -1885,7 +1886,12 @@ BaseCache::writebackBlk(CacheBlk *blk)
     // When a block is compressed, it must first be decompressed before being
     // sent for writeback.
     if (compressor) {
-        pkt->payloadDelay = compressor->getDecompressionLatency(blk);
+        pkt->headerDelay += compressor->getDecompressionLatency(blk);
+        const CompressionBlk *comp_blk =
+            dynamic_cast<const CompressionBlk *>(blk);
+        if (comp_blk && comp_blk->isCompressed()) {
+            pkt->setCompressedSize(divCeil(comp_blk->getSizeBits(), 8));
+        }
     }
 
     return pkt;
@@ -1930,7 +1936,12 @@ BaseCache::writecleanBlk(CacheBlk *blk, Request::Flags dest, PacketId id)
     // When a block is compressed, it must first be decompressed before being
     // sent for writeback.
     if (compressor) {
-        pkt->payloadDelay = compressor->getDecompressionLatency(blk);
+        pkt->headerDelay += compressor->getDecompressionLatency(blk);
+        const CompressionBlk *comp_blk =
+            dynamic_cast<const CompressionBlk *>(blk);
+        if (comp_blk && comp_blk->isCompressed()) {
+            pkt->setCompressedSize(divCeil(comp_blk->getSizeBits(), 8));
+        }
     }
 
     return pkt;
