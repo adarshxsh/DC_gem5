1be7d2e2b9bda34229d5c327b52dfabbd2c77bcd
Adarsh <udhabhaw@gmail.com>
2026-09-20 23:43:57 +0000

    mem-cache: Guard detached L1 clean lines from invalidation
    Adds a conditional guard in BaseCache::invalidateBlock to skip
    clearDetachedL1CleanBlock when invalidating blocks marked as
    detached L1 clean lines during handleEvictions.

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -1825,7 +1825,9 @@ BaseCache::invalidateBlock(CacheBlk *blk)
     // If handling a block present in the Tags, let it do its invalidation
     // process, which will update stats and invalidate the block itself
     if (blk != tempBlock) {
-        clearDetachedL1CleanBlock(regenerateBlkAddr(blk), blk->isSecure());
+        if (!blk->isDetachedL1Clean()) {
+            clearDetachedL1CleanBlock(regenerateBlkAddr(blk), blk->isSecure());
+        }
         tags->invalidate(blk);
     } else {
         tempBlock->invalidate();
