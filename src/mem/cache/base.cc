4c72f76bc1a72de84b61dfa1f665298622f1ba6e
Adarsh <udhabhaw@gmail.com>
2026-09-20 16:20:24 +0000

    mem-cache: format queue-adaptive decompression files
    Apply clang-format to queue-adaptive decompression files.

--- src/mem/cache/base.cc
+++ src/mem/cache/base.cc
@@ -82,7 +82,7 @@ BaseCache::CacheResponsePort::CacheResponsePort(const std::string &_name,
 
 BaseCache::BaseCache(const BaseCacheParams &p, unsigned blk_size)
     : ClockedObject(p),
-      cpuSidePort (p.name + ".cpu_side_port", *this, "CpuSidePort"),
+      cpuSidePort(p.name + ".cpu_side_port", *this, "CpuSidePort"),
       memSidePort(p.name + ".mem_side_port", this, "MemSidePort"),
       accessor(*this),
       mshrQueue("MSHRs", p.mshrs, 0, p.demand_mshr_reserve, p.name),
@@ -96,7 +96,7 @@ BaseCache::BaseCache(const BaseCacheParams &p, unsigned blk_size)
       writeAllocator(p.write_allocator),
       writebackClean(p.writeback_clean),
       tempBlockWriteback(nullptr),
-      writebackTempBlockAtomicEvent([this]{ writebackTempBlockAtomic(); },
+      writebackTempBlockAtomicEvent([this] { writebackTempBlockAtomic(); },
                                     name(), false,
                                     EventBase::Delayed_Writeback_Pri),
       blkSize(blk_size),
@@ -1385,11 +1385,13 @@ BaseCache::isQueueCongested() const
     if (mshrQueueThrottlingThreshold > 0) {
         if (mshrQueueThrottlingThreshold <= 100) {
             if (mshrQueue.capacity() > 0 &&
-                (mshrQueue.occupancy() * 100 / mshrQueue.capacity()) >= mshrQueueThrottlingThreshold) {
+                (mshrQueue.occupancy() * 100 / mshrQueue.capacity()) >=
+                    mshrQueueThrottlingThreshold) {
                 return true;
             }
             if (writeBuffer.capacity() > 0 &&
-                (writeBuffer.occupancy() * 100 / writeBuffer.capacity()) >= mshrQueueThrottlingThreshold) {
+                (writeBuffer.occupancy() * 100 / writeBuffer.capacity()) >=
+                    mshrQueueThrottlingThreshold) {
                 return true;
             }
         } else {
@@ -1404,7 +1406,7 @@ BaseCache::isQueueCongested() const
 }
 
 Cycles
-BaseCache::getEffectiveDecompressionLatency(const CacheBlk* blk) const
+BaseCache::getEffectiveDecompressionLatency(const CacheBlk *blk) const
 {
     if (!compressor || isQueueCongested()) {
         return Cycles(0);
